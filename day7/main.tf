cc42d7e9a59a2f992e0828cd76aac1b72f3cb076
ntnsharma0001@gmail.com <Abhishek Sharma>
2025-07-05 15:07:10 +0530

    file7 with dynamodb and s3 concept using locking mechanism

---
+++ day7/main.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "mynewvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "mynewvpc"
+  } 
+}
+ resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.mynewvpc.id
+  cidr_block = "10.0.1.0/24"
+  depends_on = [aws_vpc.mynewvpc]
+  tags = {
+    Name = "subnet1"
+  }
+ }
+ resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.mynewvpc.id
+  cidr_block = "10.0.2.0/24"
+  depends_on = [aws_vpc.mynewvpc]
+  tags = {
+    Name = "subnet2"
+  }
+ }

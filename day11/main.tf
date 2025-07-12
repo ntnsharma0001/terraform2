7152a2340a20b0e6977590371136fa0a61588b41
ntnsharma0001@gmail.com <Abhishek Sharma>
2025-07-12 15:47:05 +0530

    module concept

---
+++ day11/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type # if we give any data here, it will override the default value in variables.tf and also in module .tf
+   #means take data from here not consider as a backup
+}
+
+resource "aws_s3_bucket" "name" {
+  bucket = var.buck_name
+}
---
+++ day11/variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = ""
+}
+
+variable "buck_name" {
+  default=""
+}

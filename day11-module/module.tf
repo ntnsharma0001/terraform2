c0627fcf6980ea366baddbf87f4b750d897eb1f5
ntnsharma0001@gmail.com <Abhishek Sharma>
2025-07-12 15:47:45 +0530

    day11 is a template for this module

---
+++ day11-module/module.tf
@@ -0,0 +1,8 @@
+module "myownmodule" {
+  source        = "../day11" # Adjust the path as necessary, means onestep backon day11 folder
+  # we can also take this source as a template from anywhere aven also from github ,
+  # but just need to give the template location 
+  ami           = "ami-06c8f2ec674c67112"
+  instance_type = "t2.micro"
+  buck_name   = "ababshd"
+}
